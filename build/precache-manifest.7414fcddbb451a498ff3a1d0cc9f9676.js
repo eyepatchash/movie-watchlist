3ea6555e929ba25fc6c4c5a1b6f0972206bdd9b0
eyepatchash <aswanthnair99@gmail.com>
2021-04-23 06:09:05 +0530

    deploy 2nd

---
+++ build/precache-manifest.7414fcddbb451a498ff3a1d0cc9f9676.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5d71314c26c1cb72cb230fe124d79f9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bbf62bf58111f49065c4",
+    "url": "./static/css/2.5e3c0b62.chunk.css"
+  },
+  {
+    "revision": "54443fe8e551e1c37b49",
+    "url": "./static/css/main.16dbc0ad.chunk.css"
+  },
+  {
+    "revision": "bbf62bf58111f49065c4",
+    "url": "./static/js/2.8f52a41c.chunk.js"
+  },
+  {
+    "revision": "611b01ea38d8b31b5d98",
+    "url": "./static/js/3.a15df428.chunk.js"
+  },
+  {
+    "revision": "54443fe8e551e1c37b49",
+    "url": "./static/js/main.c41bf158.chunk.js"
+  },
+  {
+    "revision": "f916dd33b348f4936e14",
+    "url": "./static/js/runtime~main.ba5da79e.js"
+  }
+]);
--- src/views/components/Footer/Footer.js
+++ src/views/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.scss";
 const Footer = props => (
   <footer className="footer text-center w-100 mt-4 mb-4">
       &copy; {new Date().getFullYear()}, made with{" "}
-      <i className="fa fa-heart heart" /> 
+      <i className="fa fa-heart heart" /> by <a href="http://github.com/eyepatchash">eyepatch</a>
   </footer>
 );
 
